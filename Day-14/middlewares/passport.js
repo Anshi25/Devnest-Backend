a27bb6f7cfaac505a72eb2ea68903781eff624f5
Anshi25 <85192826+Anshi25@users.noreply.github.com>
2021-10-14 20:27:48 +0530

    added

---
+++ Day-14/middlewares/passport.js
@@ -0,0 +1,8 @@
+const User = require("../models/mongo");
+const { SECRET } = require("../config");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: SECRET,
+};
